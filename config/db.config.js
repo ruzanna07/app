c538099e05a0b7ae751d9ccf1067f9fddca28cbb
ruzanna07 <xachatryanruzanna5>
2022-11-27 23:34:01 +0400

    create REST API CRUD

---
+++ config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "root",
+    DB: "postgres",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+ let corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db =require("./models");
+
+db.sequelize.sync();
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./routes/tutorial.routes")(app)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,() => {
+    console.log(`server listening on port:${PORT}`);
+});
